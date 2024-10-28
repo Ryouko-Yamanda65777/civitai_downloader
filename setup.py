9d791ceb3a31c70fefddee6b8c2097565e3ce36e
Ryouko <153080407+Ryouko-Yamanda65777@users.noreply.github.com>
2024-10-29 02:12:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='civitai-downloader',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[],
     entry_points={
